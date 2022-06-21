6e853e7afe3b85451ef49e2d121d78e9a170f098
Thang Nguyen <huuthang40019@gmail.com>
2022-06-21 22:54:34 +0700

    first try

--- challenge_1/problem.py
+++ challenge_1/problem.py
@@ -7,13 +7,13 @@ from solution import fix_mask
 
 
 if __name__ == "__main__":
-    shape = (3000, 5000)
+    shape = (1280, 720)
     bin_mask, bin_mask_labels, bin_mask_split = generate_input(shape)
-
+    bin_mask, bin_mask_split = bin_mask.astype(np.uint8) * 255, bin_mask_split.astype(np.uint8) * 255
     # plt.imshow(bin_mask)
     # plt.show()
-    cv2.imwrite("bin_mask.png", bin_mask.astype(np.uint8) * 255)
-    cv2.imwrite("bin_mask_split.png", bin_mask_split.astype(np.uint8) * 255)
-    cv2.imwrite("bin_mask_labels.png", bin_mask_labels * 40)
-    fixed_mask = fix_mask(bin_mask, bin_mask_split)
-    cv2.imwrite("fixed_mask.png", fixed_mask.astype(np.uint8) * 255)
+    cv2.imwrite("test_bin_mask.png", bin_mask)
+    cv2.imwrite("test_bin_mask_split.png", bin_mask_split)
+    cv2.imwrite("test_bin_mask_labels.png", bin_mask_labels * 40)
+    fixed_mask = fix_mask(bin_mask, bin_mask_split, bin_mask_labels)
+    # cv2.imwrite("fixed_mask.png", fixed_mask.astype(np.uint8) * 255)
--- challenge_1/solution.py
+++ challenge_1/solution.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import cv2
 
+def fix_mask(bin_mask, bin_mask_split, bin_mask_labels):
+    for cluster in np.unique(bin_mask_labels)[1:]:
+        bin_cluster = np.where(bin_mask_labels == cluster, 255, 0).astype(np.uint8)
+        print(bin_mask_split.shape, bin_cluster.shape)
 
-def fix_mask(bin_mask, bin_mask_strict):
+        bin_cluster = cv2.bitwise_and(bin_mask_split, bin_cluster)
+        contours, _ = cv2.findContours(bin_cluster, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        print(len(contours))
 
-    return bin_mask_strict
+        cv2.imshow("debug", bin_cluster.astype(np.uint8))
+        cv2.waitKey(0)
+
+    return bin_mask_split
